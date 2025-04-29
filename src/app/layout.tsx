8708b390e0c70fba492fa12a652198f343cff033
luck-tech <luck@imai.am>
2025-04-29 16:00:49 +0900

    ファイル名を変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/lib/auth-context";
-import AuthChecker from "@/lib/AuthChecker";
+import AuthChecker from "@/lib/auth-checker";
 import Header from "@/components/layout/header";
 
 const geistSans = Geist({
