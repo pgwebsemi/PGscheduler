0fc65dd76dd58048be7ce26afe9783b4ec410548
ryu sugiyama <2005ryu427@gmail.com>
2025-05-24 23:38:04 +0900

    import

---
+++ src/app/schedule/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import { mockschedule as initialMember } from "../mock/schedule";
